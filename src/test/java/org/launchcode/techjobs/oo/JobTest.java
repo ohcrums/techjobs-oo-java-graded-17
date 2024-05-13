1398e0421ee40ecbd9f13070d40810cbaafebefa
ohcrums <149456208+ohcrums@users.noreply.github.com>
2024-05-13 17:45:25 -0500

    setup

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -1,5 +1,19 @@
 package org.launchcode.techjobs.oo;
 
+import org.junit.Test;
+
 public class JobTest {
     //TODO: Create your unit tests here
+    @Test
+    public void testJobsForEquality() {
+
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+    }
+
+    @Test
+    public void testSettingJobId() {
+    }
 }
